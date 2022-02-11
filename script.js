119afd734c52b0393a9803933083dd508b5f20b6
bp <bhupi98.bs@gmail.com>
2022-02-10 19:30:57 -0800

    Added math functions

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+//buttons
 const tapCalc=document.querySelector('#calcTap');
 const drillCalc=document.querySelector('#calcDrill');
 const endMillCalc=document.querySelector('#calcEndMill');
 
-// todo: Add all the other elements and add math functions
+//tap inputs
+const sfmTap=document.querySelector('#sfmTap');
+const diameterTap=document.querySelector('#diameterTap');
+const tapPitch=document.querySelector('#tapPitch');//TPI
+//tap outputs
+const tapRPMOutput=document.querySelector('#tapRPMOutput');
+const tapGradientOutput=document.querySelector('#tapGradientOutput');
 
+//drill inputs
+const drillIPR=document.querySelector('#drillIPR');
+//drill outputs
+const drillZfeed=document.querySelector('#drillZFeed');
+
+//endMill inputs
+const endMillFeed=document.querySelector('#endMillFeed');
+//endmill outputs
+const millZFeed=document.querySelector('#millZFeed');
 
 tapCalc.addEventListener("click", ()=>{
-    console.log("He tapped me right here");
+    let rpm=(sfmTap.value*97.028)/diameterTap.value;
+    let gradient=(1/tapPitch.value)*25.4;
+    tapRPMOutput.textContent=rpm;
+    tapGradientOutput.textContent=gradient;
 })
 
 drillCalc.addEventListener("click", ()=>{
-    console.log("He drilled me right here");
+    let drillFeed=drillIPR.value*25.40;
+    drillZfeed.textContent=drillFeed;
 })
-
+//inch per minute to m/min
 endMillCalc.addEventListener("click", ()=>{
-    console.log("He endmilled me right here");
+    let millFeed=endMillFeed.value/39.37;
+    millZFeed.textContent=millFeed;
 })
